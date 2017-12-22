df3def5d5fee398ea9566c9dc99d1b58e8714a15
topza5166 <topzaza5166@gmail.com>
2017-12-23 04:50:33 +0700

    add GlideModule to use GlideApp

---
+++ app/src/main/java/com/vertice/teepop/liveat500pxkotlin/dependency/GlideModule.java
@@ -0,0 +1,12 @@
+package com.vertice.teepop.liveat500pxkotlin.dependency;
+
+import com.bumptech.glide.module.AppGlideModule;
+
+/**
+ * Created by topza on 12/23/2017.
+ */
+
+@com.bumptech.glide.annotation.GlideModule
+public class GlideModule extends AppGlideModule{
+
+}
--- app/src/main/java/com/vertice/teepop/liveat500pxkotlin/view/DataBinder.java
+++ app/src/main/java/com/vertice/teepop/liveat500pxkotlin/view/DataBinder.java
@@ -3,8 +3,8 @@ package com.vertice.teepop.liveat500pxkotlin.view;
 import android.content.Context;
 import android.databinding.BindingAdapter;
 import android.widget.ImageView;
-
-import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.vertice.teepop.liveat500pxkotlin.dependency.GlideApp;
 
 /**
  * Created by topza on 12/23/2017.
@@ -22,7 +22,11 @@ public class DataBinder {
     @BindingAdapter("imageUrl")
     public static void setImageUrl(ImageView imageView, String url) {
         Context context = imageView.getContext();
-        Glide.with(context).load(url).into(imageView);
+        GlideApp.with(context)
+                .load(url)
+                .thumbnail(0.1f)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(imageView);
     }
 
 }
